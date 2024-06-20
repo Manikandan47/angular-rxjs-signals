f32a3240e7c5d4196947c71cd84d1f3ea185b9b9
Kandy Man <kandy47@gmail.com>
2024-06-19 23:43:55 -0600

    updated the product service with new method

--- apm-begin/src/app/products/product.service.ts
+++ apm-begin/src/app/products/product.service.ts
@@ -1,4 +1,7 @@
-import { Injectable } from '@angular/core';
+import { HttpClient } from '@angular/common/http';
+import { Injectable, inject } from '@angular/core';
+import { Observable, tap } from 'rxjs';
+import { Product } from './product';
 
 @Injectable({
   providedIn: 'root'
@@ -7,4 +10,12 @@ export class ProductService {
   // Just enough here for the code to compile
   private productsUrl = 'api/products';
 
+  // another way of injecting the service(dependency injection)
+  private http = inject(HttpClient);
+  
+  getProducts(): Observable<Product[]> {
+    return this.http.get<Product[]>(this.productsUrl).pipe(
+      tap(()=> console.log('In Http.get pipeline'))
+    );
+  }
 }
